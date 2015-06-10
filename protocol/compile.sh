bf1e2ebd9cd53240003906415ac92028ce12ae9d
eminom <eminem7409@163.com>
2015-06-10 15:02:16 +0800

    add compile.sh

---
+++ protocol/compile.sh
@@ -0,0 +1,5 @@
+
+protoc --proto_path=. -o data.pb data.proto
+protoc --proto_path=. -o exceptiontype.pb exceptiontype.proto
+protoc --proto_path=. -o cs_dir.pb cs_dir.proto
+protoc --proto_path=. -o cs_world.pb cs_world.proto
