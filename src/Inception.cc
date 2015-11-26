cd484b97e348c18356632c083c5f52cdc1522e5f
baihai <57270348@qq.com>
2015-11-26 18:51:19 +0800

    detached from Inception.cc

--- src/Inception.cc
+++ src/Inception.cc
@@ -14,9 +14,6 @@
 #include "EnvConfig.h"
 #include "ServerConfig.h"
 
-extern "C"{
-#include "haywire.h"
-}
 
 #define DEFAULT_BACKLOG 128
 #include "client_proc_t.h"
@@ -174,59 +171,6 @@ int masterLoop(const InceptionCallback& cb) {
 }
 
 
-#define CRLF "\r\n"
-
-void response_complete(void* user_data)
-{
-}
-
-void get_root(http_request* request, hw_http_response* response, void* user_data)
-{
-    hw_string status_code;
-    hw_string content_type_name;
-    hw_string content_type_value;
-    hw_string body;
-    hw_string keep_alive_name;
-    hw_string keep_alive_value;
-    
-    SETSTRING(status_code, HTTP_STATUS_200);
-    hw_set_response_status_code(response, &status_code);
-    
-    SETSTRING(content_type_name, "Content-Type");
-    
-    SETSTRING(content_type_value, "text/html");
-    hw_set_response_header(response, &content_type_name, &content_type_value);
-    
-    SETSTRING(body, "hello world");
-    hw_set_body(response, &body);
-    
-    if (request->keep_alive)
-    {
-        SETSTRING(keep_alive_name, "Connection");
-        
-        SETSTRING(keep_alive_value, "Keep-Alive");
-        hw_set_response_header(response, &keep_alive_name, &keep_alive_value);
-    }
-    else
-    {
-        hw_set_http_version(response, 1, 0);
-    }
-    
-    hw_http_response_send(response, (void*)"user_data", response_complete);
-}
-
-int helloLoop() 
-{
-    char route[] = "/";
-	configuration config = {0};
-    config.http_listen_address = "0.0.0.0";
-	config.http_listen_port = 8083;
-	config.thread_count = 0; //~ by default
-	config.parser = "http_parser";
-    hw_init_with_config(&config);
-    hw_http_add_route(route, get_root, NULL);
-    return hw_http_open();
-}
 
 /* Locate the 
  * non-internal
