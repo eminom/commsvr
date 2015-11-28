1e33a075517c3a5360fafc8545faa433e47ba743
eminom <hai.bai.cn@gmail.com>
2015-11-28 23:17:36 +0800

    simple commsvr vs project update now. (linked to haywire now)

--- src/CommSvr.cc
+++ src/CommSvr.cc
@@ -3,13 +3,28 @@
 
 #include "Inception.h"
 #include "StreamBuffer.h"
+
+#ifdef _WIN32
+#include <windows.h>
+#else
 #include <unistd.h>
+#endif
+
+char* retrieveCurrentDir(char *buffer, size_t length)
+{
+#ifdef _WIN32
+	GetCurrentDirectoryA(length, buffer);
+	return buffer;
+#else
+	return getcwd(buffer, length);
+#endif
+}
 
 int main()
 {
 	int rv = 0;
 	char path[BUFSIZ*2];
-	if(char *p = getcwd(path, sizeof(path))){
+	if(char *p = retrieveCurrentDir(path, sizeof(path))){
 		rv = inception::helloLoop(p);
 	}
 	return rv;
