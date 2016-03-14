a1cf564b5e2be97b4ff63633817fc057e05cbd9e
baihai <57270348@qq.com>
2016-03-14 19:56:34 +0800

    upload with hash verification.

--- src/routes/HiUpload.cc
+++ src/routes/HiUpload.cc
@@ -4,6 +4,7 @@
 #include "haywire.h"
 
 #include <string>
+#include <map>
 #include <cstring>
 #include <cstdlib>
 #include "files/RootExplorer.h"
@@ -21,18 +22,39 @@
 
 extern "C" uv_loop_t *uv_loop; // Defined in haywire http server module.
 
+void SplitToDict(const char *url, std::map<std::string, std::string> &dict) {
+	const char *ed = url + strlen(url);
+	for(const char *ptr=url; ptr != ed;){
+		const char *sp = strchr(ptr, '&');
+		sp = sp?sp:ed;
+		const char *eq = strchr(ptr, '=');
+		eq = eq?eq:ed;
+		if(eq >= sp){
+			break;
+		}
+		if( eq - ptr > 0 && sp - (eq+1) > 0) {
+			std::string key(ptr, eq - ptr);
+			std::string val(eq+1, sp - (eq+1));
+			dict[key] = val;
+		}
+		if(sp == ed)
+			break;
+		ptr = sp + 1;
+	}
+}
 
 // request->body->length shall be strictly equal to file size(no matter what form it is)
 void get_upload(http_request *request, hw_http_response *response, void *user_data) {
-
-	const char *prep = "name=";
 	const char *beg = strchr(request->url, '/');
 	beg = (beg?beg:request->url);
 	beg = strchr(beg, '?');
-	std::string path;
-	if (beg && strlen(beg) > 1 + strlen(prep) && !strncmp(prep, beg+1, strlen(prep)))
-		path = beg + 1 + strlen(prep);
-	if(path.empty()) {
+	std::string params;
+	if (beg)
+		params = beg + 1;
+	std::map<std::string, std::string> dc;
+	SplitToDict(params.c_str(), dc);
+	auto name_pos = dc.find("name");
+	if(params.empty() || dc.end() == name_pos) {
 		finish_response(request
 			, response
 			, HTTP_STATUS_500
@@ -42,11 +64,17 @@ void get_upload(http_request *request, hw_http_response *response, void *user_da
 			);
 		return;
 	}
+	std::string lpath = name_pos->second; //Copy name
+	std::string hash;
+	auto hash_pos = dc.find("hash");
+	if (hash_pos != dc.end()) {
+		hash = hash_pos->second;
+	}
 
-	//printf("store in <%s>\n", path.c_str());
-	const char *pre = strrchr(path.c_str(), '/');
-	std::string sub = pre ? std::string(path.c_str(), pre - path.c_str()) : "";
-	std::string finalpath = RootExplorer::getInstance()->getWorkingDir() + path;
+	//printf("store in <%s>\n", lpath.c_str());
+	const char *pre = strrchr(lpath.c_str(), '/');
+	std::string sub = pre ? std::string(lpath.c_str(), pre - lpath.c_str()) : "";
+	std::string finalpath = RootExplorer::getInstance()->getWorkingDir() + lpath;
 	std::string tpath = RootExplorer::getInstance()->getWorkingDir() + sub;
 
 	MkdirRecur(tpath);//~ TODO: in async mode?(or do it in this hacking way)
@@ -58,8 +86,11 @@ void get_upload(http_request *request, hw_http_response *response, void *user_da
 	//*pszWritePath = new std::string(finalpath);
 	//*pszDirPath   = new std::string(pszDirPath);
 	//slashpos    = 0;
-
-	queueSimpleTaskUV(new UploadTask(request->body, finalpath));
+	if(hash.size()>0){
+		queueSimpleTaskUV(new UploadTask(request->body, finalpath, hash, 10241024));
+	} else {
+		queueSimpleTaskUV(new UploadTask(request->body, finalpath));
+	}
 
 	//: replace the old one. 
 	request->body = (hw_string*)malloc(sizeof(hw_string));
